4b69ffeee8f2320adc932e8eaee668fe35530f61
lane-c-wagner <lane.c.wagner@gmail.com>
2018-09-15 10:31:16 -0600

    validate alias

--- opencap.go
+++ opencap.go
@@ -3,6 +3,7 @@ package opencap
 import (
 	"errors"
 	"net"
+	"strings"
 )
 
 // GetHost returns the highest priority opencap host URL at a given
@@ -26,4 +27,34 @@ func GetHost(domain string) (string, error) {
 	}
 
 	return target, nil
+}
+
+// ValidateAlias returns the username, domain, and error
+func ValidateAlias(alias string) (string, string, error) {
+	split := strings.Split(alias, "$")
+	if len(split) != 2 {
+		return "", "", errors.New("Invalid seperator use in alias")
+	}
+	username := split[0]
+	domain := split[1]
+	if len(username) < 1 {
+		return "", "", errors.New("No username in alias")
+	}
+	if len(domain) < 1 {
+		return "", "", errors.New("No domain in alias")
+	}
+
+	domainParts := strings.Split(domain, ".")
+	if len(domainParts) != 2 {
+		return "", "", errors.New("Invalid domain in alias")
+	}
+	domainName := domainParts[0]
+	ext := domainParts[1]
+	if len(domainName) < 1 {
+		return "", "", errors.New("Invalid domain in alias")
+	}
+	if len(ext) < 1 {
+		return "", "", errors.New("Invalid domain in alias")
+	}
+	return username, domain, nil
 }
--- opencap_test.go
+++ opencap_test.go
@@ -11,3 +11,22 @@ func TestGetHost(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, "opencap.ogdolo.com", url)
 }
+
+func TestValidateAlias(t *testing.T) {
+	username, domain, err := ValidateAlias("donate$ogdolo.com")
+	assert.Equal(t, "donate", username)
+	assert.Equal(t, "ogdolo.com", domain)
+	assert.Nil(t, err)
+
+	_, _, err = ValidateAlias("donateogdolo.com")
+	assert.NotNil(t, err)
+
+	_, _, err = ValidateAlias("donateogd$olocom")
+	assert.NotNil(t, err)
+
+	_, _, err = ValidateAlias("donateogdolocom")
+	assert.NotNil(t, err)
+
+	_, _, err = ValidateAlias("donateogdolo$com.")
+	assert.NotNil(t, err)
+}
